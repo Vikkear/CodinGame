ca0db4e9b22b1064ec1816beae0b746351585575
Victor Bonnier <Vikkear@hotmail.com>
2019-06-01 21:23:02 +0200

    100% completion javascript defib

--- Defibrillators.js
+++ Defibrillators.js
@@ -3,14 +3,40 @@
  * the standard input according to the problem statement.
  **/
 
+function calcDistance(longtitudeA, latitudeA, longtitudeB, latitudeB) {
+  longtitudeA = parseFloat(longtitudeA.replace(",", "."));
+  longtitudeB = parseFloat(longtitudeB.replace(",", "."));
+  latitudeA = parseFloat(latitudeA.replace(",", "."));
+  latitudeB = parseFloat(latitudeB.replace(",", "."));
+
+  let x = (longtitudeB - longtitudeA) * Math.cos((latitudeA + latitudeB) / 2);
+  let y = latitudeB - latitudeA;
+
+  return Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2)) * 6371;
+}
+
 const LON = readline();
 const LAT = readline();
 const N = parseInt(readline());
+let closest = 10000;
+let descriptionOfClosest;
+
 for (let i = 0; i < N; i++) {
   const DEFIB = readline();
+
+  // Split DEFIB seperated by ";", [4] = Longitude, [5] = Latitude
+  let defibSplit = DEFIB.split(";");
+  let newDistance = calcDistance(LON, LAT, defibSplit[4], defibSplit[5]);
+
+  if (N === 1) {
+    descriptionOfClosest = defibSplit[1];
+  } else if (newDistance < closest) {
+    closest = newDistance;
+    descriptionOfClosest = defibSplit[1];
+  }
 }
 
 // Write an action using console.log()
 // To debug: console.error('Debug messages...');
 
-console.log("answer");
+console.log(descriptionOfClosest);
